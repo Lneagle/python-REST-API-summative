e75cd20bc7d2f201049fa04a53c1dc680afe4e6e
Lacey Murphy <lneagle@gmail.com>
2025-09-13 12:39:05 -0400

    modularized API search

--- app.py
+++ app.py
@@ -2,39 +2,12 @@
 
 from flask import Flask, jsonify, request
 from flask_cors import CORS
-import requests
+from utils.search import search_for_products
 
 app = Flask(__name__)
 CORS(app)
 
 products = []
-
-def search_for_products(keyword):
-    product_name = keyword
-    product_brands = ''
-    product_ingredients = ''
-    api_url = f'https://world.openfoodfacts.org/cgi/search.pl?search_terms={keyword}&lc=en&json=1&fields=product_name,brands,ingredients_text&page_size=1'
-    
-    response = requests.get(api_url)
-    if response.status_code == 200:
-        data = response.json()
-        if data['count'] > 0:
-            products = data.get('products')
-            if products:
-                product_info = products[0]
-                name = product_info.get('product_name')
-                brands = product_info.get('brands')
-                ingredients = product_info.get('ingredients_text')
-                if name:
-                    product_name = name
-                if brands:
-                    product_brands = brands
-                if ingredients:
-                    product_ingredients = ingredients
-    else:
-        print(response)         
-    return (product_name, product_brands, product_ingredients)
-
 
 @app.route('/inventory', methods=['GET'])
 def get_inventory():
@@ -52,7 +25,7 @@ def add_product():
     product_quantity = data.get('quantity')
     product_id = max((p['id'] for p in products), default=0) + 1
     
-    (product_name, product_brands, product_ingredients) = search_for_products(search_term)
+    (product_name, product_brands, product_ingredients) = search_for_products(search_term, ['product_name', 'brands', 'ingredients_text'])
     
     new_product = {"id": product_id, "name": product_name, "brands": product_brands, "ingredients": product_ingredients, "quantity": product_quantity}
     
@@ -79,7 +52,7 @@ def delete_product(id):
     if product_index == None:
         return ('Product not found', 404)
     products.pop(product_index)
-    
+
     return ('Product deleted', 204)
 
 if __name__ == '__main__':
